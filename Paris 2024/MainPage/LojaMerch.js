ec99f96517f59d9337bdbd3af92ca40381789444
Martim Gil <martimgil@ua.pt>
2024-12-14 21:56:51 +0000

    updates na loja, a tratar do backend, frontend funcional

--- Paris 2024/MainPage/LojaMerch.js
+++ Paris 2024/MainPage/LojaMerch.js
@@ -1,34 +1,54 @@
-let inputPrecoTotal = $("#total");
-let inputQtdTotal = $("quantidades");
-let precoTotal = 0;
-let qtdTotal = 0;
-
-
 function addProduct(number) {
-    let quantidadeProdutoSelecionado = $("#qty" + number);
+    let quantidadeProdutoSelecionado = document.getElementById("qty" + number);
     quantidadeProdutoSelecionado.value++;
     calculate();
+
+    let Cart = JSON.parse(window.localStorage.getItem('Cart')) || [];
+    let productElement = document.getElementById(`qty${number}`);
+    let productNameElement = productElement.parentElement.querySelector('h5');
+    let productPriceElement = document.getElementById(`price${number}`);
+    let product = {
+        name: productNameElement.innerText,
+        quantity: parseInt(productElement.value),
+        price: parseFloat(productPriceElement.value),
+        number: parseFloat(number)
+
+    };
+
+    let existingProductIndex = Cart.findIndex(item => item.name === product.name);
+
+    if (existingProductIndex !== -1) {
+        Cart[existingProductIndex].quantity = product.quantity;
+    } else {
+        Cart.push(product);
+    }
+    window.localStorage.setItem('Cart', JSON.stringify(Cart));
+    console.log(Cart);
+
 }
 
 function calculate() {
     let precAtual, qtdAtual;
-    precoTotal = 0;
-    qtdTotal = 0;
+    let precoTotal = 0;
+    let qtdTotal = 0;
 
-    for (let i = 1; i <= 6; i++) {
-        precAtual = parseFloat($('#price' + i).val());
-        qtdAtual = parseFloat($('#qty' + i).val());
+    for (let i = 1; i <= 15; i++) {
+        precAtual = parseFloat(document.getElementById('price' + i).value);
+        qtdAtual = parseFloat(document.getElementById('qty' + i).value);
         precoTotal += precAtual * qtdAtual;
         qtdTotal += qtdAtual;
     }
 
-    if (precoTotal> 100) {
-        precoTotal  = precoTotal * 0.95;
+    if (precoTotal > 100) {
+        precoTotal = precoTotal * 0.95;
     }
 
-    if (qtdTotal>= 5) {
-        precoTotal  = precoTotal * 0.95;
+    if (qtdTotal >= 5) {
+        precoTotal = precoTotal * 0.95;
     }
+
+    let inputQtdTotal = document.getElementById('quantidades');
+    let inputPrecoTotal = document.getElementById('total');
 
     inputQtdTotal.innerText = qtdTotal;
     inputPrecoTotal.innerText = precoTotal.toFixed(2);
@@ -39,16 +59,17 @@ function valid() {
         alert("Erro o carrinho está vazio");
         return false;
     } else {
-        return true
+        return true;
     }
 }
 
 function clean() {
-    for (let i = 1; i <= 6; i++) {
-        $('#qty' + i).val(0);
+    for (let i = 1; i <= 15; i++) {
+        document.getElementById('qty' + i).value = 0;
     }
     precoTotal = 0;
     qtdTotal = 0;
     inputPrecoTotal.innerText = "0.00";
     inputQtdTotal.innerText = 0;
+    window.localStorage.removeItem('Cart');
 }
