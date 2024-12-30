932786291f1f122da923d930df561a828cd8862b
DukDani <105384861+DukDani@users.noreply.github.com>
2024-12-30 17:53:47 +0000

    Nacionalidades Detalhes

---
+++ Paris 2024/MainPage/NacionalidadesDetalhe.js
@@ -0,0 +1,83 @@
+// ViewModel Knockout
+var vm = function () {
+    var self = this; // Definir `self` adequadamente
+    console.log("ViewModel initiated...");
+
+    //--- Variáveis locais
+    self.baseUri = ko.observable('http://192.168.160.58/Paris2024/api/NOCs/');
+    self.displayName = 'Detalhes da Nacionalidade';
+    self.CompInfo = ko.observableArray([]);
+    self.CompInfo2 = ko.observableArray([]);
+    self.SName = ko.observable('');
+
+
+    self.activate = function (id) {
+        console.log('CALL: getAthlete...');
+        var composedUri = self.baseUri() + id;
+        ajaxHelper(composedUri, 'GET').done(function (data) {
+            hideLoading();
+
+            self.CompInfo(data);
+            console.log(self.CompInfo());
+        });
+    };
+
+    function ajaxHelper(uri, method, data) {
+        return $.ajax({
+            type: method,
+            url: uri,
+            dataType: 'json',
+            contentType: 'application/json',
+            data: data ? JSON.stringify(data) : null,
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log("AJAX Call [" + uri + "] Fail...");
+                hideLoading();
+                self.error(errorThrown);
+            }
+        });
+    }
+
+    function showLoading() {
+        $('#myModal').modal('show', {
+            backdrop: 'static',
+            keyboard: false
+        });
+    }
+
+    function hideLoading() {
+        $('#myModal').modal('hide');
+    }
+
+    function getUrlParameter(sParam) {
+        var sPageURL = window.location.search.substring(1),
+            sURLVariables = sPageURL.split('&'),
+            sParameterName;
+
+        for (var i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
+            if (sParameterName[0] === sParam) {
+                return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+            }
+        }
+    }
+
+    // --- start ....
+    showLoading();
+    var pg = getUrlParameter('id');
+    console.log(pg);
+    if (pg === undefined)
+        self.activate(1);
+    else {
+        self.activate(pg);
+    }
+    console.log("VM initialized!");
+};
+
+$(document).ready(function () {
+    console.log("document.ready!");
+    ko.applyBindings(new vm());
+});
+
+$(document).ajaxComplete(function () {
+    $("#myModal").modal('hide');
+});
